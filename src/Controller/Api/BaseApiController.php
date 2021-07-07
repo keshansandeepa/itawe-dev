355d58442dc8aebd5f87ef10698d40a36ed3c90a
keshansandeepa <keshansandeepa@users.noreply.github.com>
2021-07-07 16:08:25 +0000

    Fix styling

--- src/Controller/Api/BaseApiController.php
+++ src/Controller/Api/BaseApiController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class BaseApiController extends AbstractController
 {
