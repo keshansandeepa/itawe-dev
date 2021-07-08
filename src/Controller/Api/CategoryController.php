67a7bda692a00f410fae1a9eb1a5fbfee7b03d7d
Keshan Perera <stkeshansandeepa@gmail.com>
2021-07-09 01:50:16 +0530

    fixed issue

---
+++ src/Controller/Api/CategoryController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class CategoryController extends AbstractController
+{
+    private SerializerInterface $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @Route ("/api/categories", methods={"GET"})
+     */
+
+    public function index(CategoryRepository $categoryRepository)
+    {
+        return new Response(
+            $this->serializer->serialize($categoryRepository->findAll(), 'json', ['groups' => 'show_book']),
+            Response::HTTP_OK,
+            ['Content-type' => 'application/json']
+        );
+    }
+}
