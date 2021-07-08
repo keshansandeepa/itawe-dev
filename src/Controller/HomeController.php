96d052bcbe76b60c08ffe10581e8d5cb786f5149
Keshan Perera <stkeshansandeepa@gmail.com>
2021-07-09 00:57:17 +0530

    fixed issue

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,11 @@
 
 namespace App\Controller;
 
-use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    public function index(AuthorRepository $authorRepository)
+    public function index()
     {
     }
 }
